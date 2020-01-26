01684d94995a2b622689ced7b0b80a290223c475
Tera <tera@fargobose.com>
2020-01-26 13:14:25 -0800

    changes to the plugin and updating the documentation

--- README.md
+++ README.md
@@ -1,87 +1,113 @@
-# homebridge-platform-sqs
-Hombridge platform for Amazon SQS, esp Dash Buttons. [Full blogpost about it](https://fastchicken.co.nz/2017/06/25/more-homebridge-aws-iot-dash-button-sqs-broadlink-rm3-mini/)
+# homebridge-platform-azure-servicebus
+An [Azure Service Bus](https://azure.microsoft.com/en-us/services/service-bus/) plugin for homebridge. 
 
-# My setup
+This plugin allows both incoming service bus messages from a queue. These messages will trigger a switch based on the name of the accessory defined in the `config.json` as described below. 
 
-I have homebridge running on a Mac Mini on my local network, controlling 4 WeMo
-switches (lights, heaters, coffee machine) and 2 IR senders to control the Heat Pump.
+This plugin also allows different switches to trigger the sending of messages **to** a queue. These messages will contain the source button name in the message. It is advised to make these switches to a timeout switch to make them act like a button.
 
-I also have a Dash Button.
+## My setup
 
-I used the default Dash Button app on the appstore to setup the button, and have it create a basic Lambda, which normally just sends an SMS. I changed that lambda to write to an SQS queue.
+I have a homebridge running on a Raspberry Pi 4. Controlling a Nest Thermostat and this plugin. 
 
-# Installing
+## Why?
+Why on earth would someone need a service bus queue to control home automation? Service Bus seems to be a [very affordable](https://azure.microsoft.com/en-us/pricing/details/service-bus/) solution for queueing messages. I selected Service Bus due to my familiarity with it. The Cloud Based Queues are significantly more reliable than public free eventing platforms such as [If This Then That](https://ifttt.com/). 
 
-```
-npm install -g https://github.com/nicwise/homebridge-platform-sqs.git
-```
+My primary use case was connecting my home automation kit to events from Office365 for notification of upcoming meetings. 
 
-Might be coming to NPM sometime soon. Maybe.
+### Primary Workflow
 
-# Config
+1. Meeting is created in Office 365.
+2. Microsoft Flow is triggered when an upcoming meeting (**Authenticated**)
+    1. Microsoft Flow gets details of the meeting.
+    2. Microsoft Flow checks to see if the categories include the category `Critical`
+    3. If the category is contained, send a message using Service bus to my service bus queue (**Authenticated**);
+3. Queue receives Message
+4. Homebridge plugin receives message using AQMP listener.
+5. Homebridge plugin checks to see if the target accessory of the message, the ID of the accessory is simply the name. 
+      1. If the accessory is contained in the list, it will be toggled. This change is observable by HomeKit and can be used to trigger automation.
+      2. if the accessory is not found, the message will be deleted. 
 
-Look in `config-sample.json`, but you want to add a new platform, called `SQS`
+## Installation
 
-```
-"platforms": [
-    {
-      "platform": "SQS",
-      "name": "SQS",
-      "accessories": [
-        {
-          "name": "Dash Button",
-          "queueurl": "https://sqs.us-west-2.amazonaws.com/accountid/sqsqueuename",
-          "accesskey": "AWSACCESSKEY",
-          "secret": "AWSACCESSSECRET",
-          "region": "us-west-2"
-        }
-      ]
-    }
-  ]
+Install the plugin at the global scope. 
+``` bash
+sudo npm install -g https://github.com/nicwise/homebridge-platform-sqs.git
 ```
 
-You need to name it (which shows in Homekit/Home.app), give it the queue URL you got from AWS,
-and the accesskey and secret for the user you want to have the queue use. And the AWS region it's in.
+Once the plugin is installed, it must be configured. 
 
-# AWS setup
+## Config
 
-This is mostly for using it with a Dash Button, but it can be used with anything
-which will write to an SQS queue.
+This example is also contained within `/sample-config.json`.
 
-You will need to setup the SQS queue, and have something write to it, eg a Lambda
-(lambda included in here).
-
-You should make an API-only user (eg `sqs.reader`) in IAM, which has the following IAM policy applied to them:
-```
+``` json
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "Stmt19987",
-            "Effect": "Allow",
-            "Action": [
-                "sqs:GetQueueAttributes",
-                "sqs:ListQueues",
-                "sqs:ReceiveMessage",
-                "sqs:SendMessage",
-                "sqs:DeleteMessage",
-                "sqs:PurgeQueue"
-            ],
-            "Resource": [
-                "arn:aws:sqs:<the rest of the URN of your queue>"
-            ]
-        }
-    ]
+			"platform": "AzureServiceBus",
+			"name": "ServiceBusButtons",
+			"incommingMessageConnectionString": "connectionStringFromAzurePortalOrCmd",
+			"outgoingMessageConnectionString": "connectionStringFromAzurePortalOrCmd",
+			"incomingQueueName": "The name of the queue. Also specified in the EntityPath",
+			"outgoingQueueName": "The name of the queue. Also specified in the EntityPath",
+			"incomingSwitches": [
+				{
+					"name": "TestButton1"
+				},
+				{
+					"name": "TestButton2",
+					"isTimeoutButton": true,
+					"buttonTimeout": 1500
+				}
+			],
+			"outgoingButtons": [
+				{
+					"name": "foo",
+					"isTimeoutButton": true,
+					"buttonTimeout": 500
+				}
+			]
+		}
+```
+
+You must use a `SharedAccessSignature` to send or receive messages from a service bus queue. The two queues (`incoming` and `outgoing`) must be individually specified. In addition, the queue name for each must match.
+
+### Incoming Switches
+Incoming switches are accessories that will respond to messages on the incoming queue. They are specified in an array in the `incomingSwitches` section of the config.
+
+Example Message:
+This message will cause the switch to toggle, and due to its timeout property, its state will reset in `1500ms`. Using the `isTimeoutButton` property will cause the switch to act like a sticky button. 
+``` json
+{
+  "target": "TestButton2"
 }
 ```
-> I think you only NEED `sqs:ReceiveMessage`, `sqs:DeleteMessage` and `sqs:PurgeMessage` really. To send the SQS message, the role the lambda is in needs `sqs:SendMessage`
 
-From this user, you can get the accesskey and secret, which goes into the config,
-along with the URL and region.
+### Outgoing Buttons
+Outgoing Buttons are accessories that will cause a service bus message to be sent to the outgoing queue. The message will simply contain the `name` of the accessory that was pushed. These accessories will always timeout after `500ms` and will act like buttons. Unfortunately as of this version, buttons dont work as expected. 
 
-> NOTE: treat the accesskey and secret as a username/password - keep it safe.
+Sample Message:
+This is the sample message that will be generated.
+``` json
+{
+  "source": "Foo"
+}
+```
 
-You can test the queue by right clicking on it in the console and sending a message.
-Personally, I use a Dash Button to connect to AWS IOT, which then invokes a Lambda
-function, which sends an SQS message, which lands in here and turns lights on using
-the automations in HomeKit (you may need an AppleTV or iPad for that)
+## Azure setup
+
+This plugin expects a basic understanding of Azure and ServiceBus programming. This guide is **not** a comprehensive guide on how to use or setup servicebus. 
+
+![Service Bus Overbiew](https://docs.microsoft.com/en-us/azure/includes/media/howto-service-bus-queues/sb-queues-08.png)
+
+Please follow the [Full Guide](https://docs.microsoft.com/en-us/azure/service-bus-messaging/service-bus-quickstart-portal) to creating a service bus queue for additional information.
+
+1. Create a **Service Bus Namespace**. This namespace is a container that can contain multiple queues.
+2. Create a **Queue** for Incoming Messages in the new Namespace.
+3. Create a **SharedAccessSignature** within the queue to for authorization. It is recommended to use the queue level Shared Access Signature, instead of the root SAS signature. 
+    1. The Connection string should be placed in the `incommingMessageConnectionString` element. 
+    2. The Queue Name should be placed in the `incomingQueueName` element. This can either be copied from the portal, or from the `EntitiyPath=YOUR_QUEUE_NAME_HERE` section of the connection string sourced from a queue level SAS. 
+4. Create a **Queue** for Outgoing Messages in the new Namespace. 
+5. Create a **SharedAccessSignature** within the outgoing queue for authorization. 
+    1. The Connection string should be placed in the `outgoingMessageConnectionString` element. 
+    2. The Queue Name should be placed in the `outgoingQueueName` element. This can either be copied from the portal, or from the `EntitiyPath=YOUR_QUEUE_NAME_HERE` section of the connection string sourced from a queue level SAS. 
+
+**WARNING**: The Connection String is a *credential* and should be treated as such. Any party with the SAS connection string can send or receive messages to and from the queue.
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var Accessory, Service, Characteristic, UUIDGen;
 
 
 
-module.exports = function(homebridge) {
+module.exports = function (homebridge) {
 
   // Accessory must be created from PlatformAccessory Constructor
   Accessory = homebridge.platformAccessory;
@@ -29,19 +29,94 @@ function AzureServiceBusPlatform(log, config, api) {
 }
 
 AzureServiceBusPlatform.prototype = {
-  accessories: function(callback) {
-    //For each device in cfg, create an accessory!
-    var foundAccessories = this.config.accessories;
+  accessories: function (callback) {
+    var i;
+
+    var self = this;
+
+    this.incomingQueueName = this.config.incomingQueueName;
+    this.outgoingQueueName = this.config.outgoingQueueName;
+
+    this.log(this.incomingQueueName);
+    this.log(this.outgoingQueueName);
+
+    this.log("Creating incoming service bus message client");
+    this.incomingServiceBusClient = ServiceBusClient.createFromConnectionString(this.config.incommingMessageConnectionString);
+    this.incomingQueueClient = this.incomingServiceBusClient.createQueueClient(this.incomingQueueName);
+
+    this.log("Creating outgoing service bus message client");
+    this.outgoingServiceBusClient = ServiceBusClient.createFromConnectionString(this.config.outgoingMessageConnectionString);
+    this.outgoingQueueClient = this.outgoingServiceBusClient.createQueueClient(this.outgoingQueueName);
+
+    this.log("creatingSender");
+    this.sender = this.outgoingQueueClient.createSender();
+    this.log("finished creating sender");
+
     var myAccessories = [];
 
-    for (var i = 0; i < foundAccessories.length; i++) {
-      var accessory = new AzureServiceBusAccessory(this.log, foundAccessories[i]);
-      myAccessories.push(accessory);
-      this.log('Created ' + accessory.name + ' Accessory');
+    this.log("Pulling Config for incoming switches");
+    var foundIncomingAccessories = this.config.incomingSwitches;
+    this.log("Incoming Config = " + JSON.stringify(foundIncomingAccessories, null, 4));
+
+
+    this.log("Pulling Config for outgoing Buttons");
+    var foundOutgoingAccessories = this.config.outgoingButtons;
+    this.log("Outgoing Config = " + JSON.stringify(foundOutgoingAccessories, null, 4));
+
+    // create hashtable map of accessories based 
+    // on name for perf in large collections
+    var _accessoryMap = {};
+    var myAccessories = [];
+
+    for (var i = 0; i < foundIncomingAccessories.length; i++) {
+      var incomingAccessory = new AzureServiceBusAccessoryIncomingSwitch(this.log, foundIncomingAccessories[i]);
+      myAccessories.push(incomingAccessory);
+      _accessoryMap[incomingAccessory.name] = incomingAccessory;
+      this.log('Created ' + incomingAccessory.name + ' Accessory (Incoming)');
     }
+
+    for (var i = 0; i < foundOutgoingAccessories.length; i++) {
+      var outgoingAccessory = new AzureServiceBusAccessoryOutgoingButton(this.log, foundOutgoingAccessories[i]);
+      outgoingAccessory.sender = this.sender;
+      myAccessories.push(outgoingAccessory);
+      this.log('Created ' + outgoingAccessory.name + ' Accessory (Outgoing)');
+    }
+
     callback(myAccessories);
+
+    this.accessoryMap = _accessoryMap;
+
+    this.receiver = this.incomingQueueClient.createReceiver(ReceiveMode.receiveAndDelete);
+
+    this.log("creating message handler");
+    this.receiver.registerMessageHandler(function (brokeredMessage) {
+      self.log("message received: " + JSON.stringify(brokeredMessage.body, null, 4));
+
+      var obj = brokeredMessage.body;
+
+      // if( obj.target === self.name)
+      if (!(obj.target in self.accessoryMap)) {
+        self.log("Accessory Name '" + obj.target + "' not found, ignoring and deleting message " + brokeredMessage.id);
+        return;
+      }
+
+      self.log("Accessory " + obj.target + " found. Processing message");
+
+      var acc = self.accessoryMap[obj.target];
+
+      acc.toggleButton();
+    },
+      function (err) {
+        self.log("error occured: " + err);
+      },
+      {
+        autoComplete: true
+      });
+    this.log("finished creating message handler");
+
+
   },
-  removeAccessory: function(accessory) {
+  removeAccessory: function (accessory) {
     if (accessory) {
       this.api.unregisterPlatformAccessories("homebridge-amazondash", "AmazonDash", [accessory]);
     }
@@ -51,61 +126,53 @@ AzureServiceBusPlatform.prototype = {
 
 //an accessorary, eg a button. This one is mostly just an on/off state button.
 //SQS message toggles it, as does pressing it in the home app
-function AzureServiceBusAccessory(log, accessory) {
+function AzureServiceBusAccessoryIncomingSwitch(log, accessory) {
   this.log = log;
   this.accessory = accessory;
   this.name = this.accessory.name;
   this.buttonIsOn = false;
-  this.startListener();
+  this.buttonLastToggledState = false;
+  this.isTimeoutButton = this.accessory.isTimeoutButton === true;
+
+  if (this.isTimeoutButton) {
+    this.buttonTimeout = this.accessory.buttonTimeout;
+  }
+  else {
+    this.buttonTimeout = -1;
+  }
 }
 
-AzureServiceBusAccessory.prototype = {
-  startListener: function() {
+AzureServiceBusAccessoryIncomingSwitch.prototype = {
+  toggleButton: function () {
     var self = this;
 
-    this.queueName = this.accessory.queueName;
-    this.serviceBusClient = ServiceBusClient.createFromConnectionString(this.accessory.connectionString);
-    this.queueClient = this.serviceBusClient.createQueueClient(this.queueName);
-    this.receiver = this.queueClient.createReceiver(ReceiveMode.receiveAndDelete);
-    
-    this.receiver.registerMessageHandler(function (brokeredMessage) {
-      self.log("message received: " + brokeredMessage.body);
-    
-      var obj = brokeredMessage.body;
-
-      if(obj.target === self.name)
-      {
-        self.log("found incoming message for '" + self.name + "' which is this accessory, processing.");
-      }
-      else
-      {
-        self.log("got incoming message for another accessory ('" + self.name + "') ignoring.");
-        return;
-      }
-
-      self.toggleButton();  
-    },
-    function (err) {
-      self.log("error occured: " + err);
-    },
-    {
-      autoComplete: true
-    });
-  },
-
-  toggleButton: function() {
     //toggle the internal state of the button
     this.buttonIsOn = !this.buttonIsOn;
-    this.log(`${this.name}: SQS Button state change. New state is ${this.buttonIsOn}`);
+    this.buttonLastToggledState = this.buttonIsOn;
+    this.log(`${this.name}: Azure ServiceBus Button state change. New state is ${this.buttonIsOn}`);
     this.service.getCharacteristic(Characteristic.On).setValue(this.buttonIsOn);
+
+    if (this.isTimeoutButton === true) {
+      self.log("Found Button is timeout button. Will reset to previous state in " + this.buttonTimeout.toString() + "ms");
+
+      setTimeout(function () {
+        self.log(`${self.name}: Timeout complete`);
+        self.buttonIsOn = !self.buttonLastToggledState;
+        self.buttonLastToggledState = self.buttonIsOn;
+
+        self.log(`${self.name}: Azure ServiceBus Button state change. New state is ${self.buttonIsOn}`);
+        self.service.getCharacteristic(Characteristic.On).setValue(self.buttonIsOn);
+      },
+        self.buttonTimeout);
+    }
   },
 
-  identify: function(callback) {
+  identify: function (callback) {
     this.log("[" + this.name + "] Identify requested!");
     callback(); // success
   },
 
-  getServices: function() {
+  getServices: function () {
     //get the services this accessory supports
     //this is were we setup the button, but if it was, eg, a fan, you'd make a fan here.
 
@@ -122,7 +189,7 @@ AzureServiceBusAccessory.prototype = {
       .on('set', this.setSPState.bind(this));
 
     informationService
-      .setCharacteristic(Characteristic.Model, 'QueueButton')
+      .setCharacteristic(Characteristic.Model, 'Incoming Service Bus Message')
       .setCharacteristic(Characteristic.SerialNumber, '1.0');
 
     services.push(switchService, informationService);
@@ -133,13 +200,13 @@ AzureServiceBusAccessory.prototype = {
     return services;
   },
 
-  getSPState: function(callback) {
+  getSPState: function (callback) {
     //homekit calling into us to get the state
     this.log(`${this.name}: Get State: ${this.buttonIsOn}`);
     callback(null, this.buttonIsOn);
   },
 
-  setSPState: function(state, callback) {
+  setSPState: function (state, callback) {
 
     //homekit calling into us to set the state. state is 1 or 0
     if (state) {
@@ -147,8 +214,111 @@ AzureServiceBusAccessory.prototype = {
     } else {
       this.buttonIsOn = false;
     }
-    this.log(`${this.name}: Set State to ${this.buttonIsOn}`);
+    this.log(`${this.name}: CALL FROM HOMEKIT. Set State to ${this.buttonIsOn}`);
     callback(null, this.buttonIsOn);
+
+  }
+}
+
+
+//an accessorary, eg a button. This one is mostly just an on/off state button.
+//SQS message toggles it, as does pressing it in the home app
+function AzureServiceBusAccessoryOutgoingButton(log, accessory) {
+  this.log = log;
+  this.accessory = accessory;
+  this.name = this.accessory.name;
+  this.buttonIsOn = false;
+}
+
+AzureServiceBusAccessoryOutgoingButton.prototype = {
+  toggleButton: function () {
+    var self = this;
+
+    //toggle the internal state of the button
+    this.buttonIsOn = !this.buttonIsOn;
+    this.buttonLastToggledState = this.buttonIsOn;
+    this.log(`${this.name}: Azure ServiceBus Button state change. New state is ${this.buttonIsOn}`);
+    this.service.getCharacteristic(Characteristic.On).setValue(this.buttonIsOn);
+
+
+  },
+
+  identify: function (callback) {
+    this.log("[" + this.name + "] Identify requested!");
+    callback(); // success
+  },
+
+  getServices: function () {
+    //get the services this accessory supports
+    //this is were we setup the button, but if it was, eg, a fan, you'd make a fan here.
+
+    var services = [];
+
+    var informationService = new Service.AccessoryInformation();
+    informationService
+      .setCharacteristic(Characteristic.Manufacturer, 'Fargo Bose Security');
+
+    var switchService = new Service.Switch(this.accessory.name);
+    switchService
+      .getCharacteristic(Characteristic.On)
+      .on('get', this.getSPState.bind(this))
+      .on('set', this.setSPState.bind(this));
+
+    informationService
+      .setCharacteristic(Characteristic.Model, 'Outgoing Service Bus Message')
+      .setCharacteristic(Characteristic.SerialNumber, '1.0');
+
+    services.push(switchService, informationService);
+
+    //keep the service, so we can turn it on/off later.
+    this.service = switchService;
+
+    return services;
+  },
+
+  getSPState: function (callback) {
+    //homekit calling into us to get the state
+    this.log(`${this.name}: Get State: ${this.buttonIsOn}`);
+
+
+
+    callback(null, this.buttonIsOn);
+  },
+
+  setSPState: function (state, callback) {
+    var self = this;
+
+    //homekit calling into us to set the state. state is 1 or 0
+    if (state) {
+      this.buttonIsOn = true;
+
+      self.log("Found Button is timeout button. Will reset to previous state in 500ms");
+
+      setTimeout(function () {
+        self.log(`${self.name}: Timeout complete`);
+        self.buttonIsOn = false;
+
+        self.service.getCharacteristic(Characteristic.On).setValue(self.buttonIsOff);
+      },
+      500);
+
+      var msg = {
+        body: {
+          source: this.name
+        }
+      };
+
+      this.log("Sending service bus message");
+      this.sender.send(msg);
+
+      callback(null, this.buttonIsOn);
+    } else {
+      this.buttonIsOn = false;
+
+      callback(null, this.buttonIsOff);
+    }
+
+
 
   }
 }
